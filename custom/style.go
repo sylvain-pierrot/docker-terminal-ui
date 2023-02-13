0e0c42d80ea292573fd783ecd2723a91a32357a5
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2023-02-13 18:30:40 +0100

    refactor code

--- custom/style.go
+++ custom/style.go
@@ -5,18 +5,10 @@ import (
 )	 
 
 func CreateStyle(width, height int, color string) lipgloss.Style {
-	// var sumWidthCells int
-	// for i := 0; i < len(columns); i++ {
-	// 	sumWidthCells += columns[i].Width
-	// }
-	// if sumWidthCells > windowWidth
-
 	style := lipgloss.NewStyle().
 	BorderStyle(lipgloss.ThickBorder()).
 	BorderForeground(lipgloss.Color(color)).
 	Foreground(lipgloss.Color("#7accf8")).
-	// PaddingTop(1).
-    // PaddingBottom(1).
     PaddingLeft(1).
     PaddingRight(1).
 	Width(width)
